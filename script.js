ab91af7bf43d4ea18df7df63f65d88ed40d2c1ea
Ah Rifqi Ubaidilah <rifqi23ahmad@gmail.com>
2024-09-10 01:27:01 +0700

    fixing button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const showHeartButton = document.getElementById('showHeart');
     const heartImage = document.getElementById('heartImage');
+    const backgroundMusic = document.getElementById('backgroundMusic');
+
+    // Play background music
+    backgroundMusic.volume = 0.2; // Adjust volume as needed
+    backgroundMusic.play();
 
     showHeartButton.addEventListener('click', () => {
         heartImage.classList.toggle('hidden');
