b43c5c9b8394bffc9119dc90cd2b8d126cd00a94
Ah Rifqi Ubaidilah <rifqi23ahmad@gmail.com>
2024-09-10 00:33:08 +0700

    tambah cantik

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        // Partikel.js
+        particlesJS.load('particleBackground', 'particles.json', function() {
+            console.log('particles.js config loaded');
+        });
+    
+        // Existing code
+    });
+    
     const showHeartButton = document.getElementById('showHeart');
     const heartImage = document.getElementById('heartImage');
     const changeMessageButton = document.getElementById('changeMessage');
     const messageElement = document.getElementById('message');
+   
+    const backgroundMusic = document.getElementById('backgroundMusic');
+
+    // Play background music
+    backgroundMusic.volume = 0.2; // Adjust volume as needed
+    backgroundMusic.play();
 
     showHeartButton.addEventListener('click', () => {
         heartImage.classList.toggle('hidden');
