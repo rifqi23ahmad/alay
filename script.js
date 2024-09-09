902f516b68685b6c695b188c606af4c84e889fd1
Ah Rifqi Ubaidilah <rifqi23ahmad@gmail.com>
2024-09-10 00:38:35 +0700

    add button

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.addEventListener('DOMContentLoaded', () => {
-        // Partikel.js
-        particlesJS.load('particleBackground', 'particles.json', function() {
-            console.log('particles.js config loaded');
-        });
-    
-        // Existing code
-    });
-    
     const showHeartButton = document.getElementById('showHeart');
     const heartImage = document.getElementById('heartImage');
-    const changeMessageButton = document.getElementById('changeMessage');
-    const messageElement = document.getElementById('message');
-   
     const backgroundMusic = document.getElementById('backgroundMusic');
 
     // Play background music
@@ -23,10 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
         heartImage.classList.toggle('hidden');
     });
 
-    changeMessageButton.addEventListener('click', () => {
-        const newMessage = prompt('Masukkan pesan permintaan maaf baru:');
-        if (newMessage) {
-            messageElement.textContent = newMessage;
-        }
+    // Initialize particles
+    particlesJS.load('particleBackground', 'particles.json', function() {
+        console.log('particles.js config loaded');
     });
 });
