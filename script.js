00d6714b9fc179faf09932ed4e2be1af92a2a3a7
Ah Rifqi Ubaidilah <rifqi23ahmad@gmail.com>
2024-09-10 04:15:33 +0700

    add kalkulator

--- script.js
+++ script.js
@@ -116,3 +116,23 @@ function displayNoteHistory() {
         noteHistory.appendChild(listItem);
     });
 }
+
+// Kalkulator
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
